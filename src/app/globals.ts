4c3300e193dbd01df30303aabfd4bfcafcd73039
benjaminsmith1985 <48875437+benjaminsmith1985@users.noreply.github.com>
2020-10-15 17:49:45 -0400

    notification styled

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -5,8 +5,8 @@ import { Observable, Subject } from 'rxjs';
 @Injectable()
 export class Globals {
     expired: 0;
-    // serverlink: string = "http://localhost/complianceServer/"; 
+    serverlink: string = "http://localhost/complianceServer/"; 
     isLoading: boolean = false;
 
-    serverlink: string = "complianceServer/";
+    //serverlink: string = "complianceServer/";
 }   
